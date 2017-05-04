4ca3809bf1e6fa04b2aa761c9a89ab62f8f9a6aa
martapcosta <martapcosta@gmail.com>
2017-05-04 16:49:33 +0000

    laravel 2

--- laravel-todomvc/routes/web.php
+++ laravel-todomvc/routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('tasks', 'TasksController');
+
+Route::get('/', 'TasksController@index');
+
+//Route::get('/test{param}', function ($param) {
+  //  echo "test" . $param->with($param,'marta');
+    //return view('test');
+    //return "Hello World!!";
+//});
+
+/*Route::get('ID/{id}', function ($id) {
+  return 'ID: '. $id;
+});*/
+
+
+//Route::get('/', 'TasksController@index');
+
+//Route::get('/tasks/{id}', 'TasksController@show');
+
+//Route::resource('tasks','TasksController');
+//Route::get('tasks/{id}', 'TasksController@show')->name('task');
+//route('task', ['id' => 1]);
